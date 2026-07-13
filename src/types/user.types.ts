203ae1db8c3c7dc68a1d1d7eca59ae759458a876
Eyeni Mbu Nyellie <mbunyellie0@gmail.com>
2026-07-13 20:59:13 +0100

    admin and student dashboard

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -4,3 +4,14 @@ export interface StudentProfile {
   avatar?: string;
   streakDays: number;
 }
+
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+
+export interface RegisterPayload {
+  name: string;
+  email: string;
+  password: string;
+}
