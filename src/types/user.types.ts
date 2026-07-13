c48907c6110d60664dee1c192d4d444e70caa578
Eyeni Mbu Nyellie <mbunyellie0@gmail.com>
2026-07-13 21:28:38 +0100

    Fix TypeScript types and build errors

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -15,3 +15,36 @@ export interface RegisterPayload {
   email: string;
   password: string;
 }
+
+export interface SetRolePayload {
+  role: "student" | "tutor";
+}
+
+export interface RefreshPayload {
+  refresh_token: string;
+}
+
+export interface User {
+  id: string;
+  full_name: string;
+  email: string;
+  role?: string;
+  avatar?: string;
+}
+
+export interface AuthResponse {
+  user: User;
+  tokens: {
+    access_token: string;
+    refresh_token: string;
+  };
+}
+
+
+export interface ApiError {
+  detail:
+    | string
+    | {
+        msg: string;
+      }[];
+}
