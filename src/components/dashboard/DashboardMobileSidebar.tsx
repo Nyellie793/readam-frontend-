a09fd2da77a7288c72c6c88e64962479ea7569c5
Eyeni Mbu Nyellie <mbunyellie0@gmail.com>
2026-07-20 07:44:38 +0100

    courses are up and running

--- src/components/dashboard/DashboardMobileSidebar.tsx
+++ src/components/dashboard/DashboardMobileSidebar.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { useState } from "react";
-import { Menu, X } from "lucide-react";
+import { X } from "lucide-react";
 import { usePathname } from "next/navigation";
 import Sidebar from "./Sidebar";
 import CourseFilters from "./courses/CourseFilters";
 
-export default function DashboardMobileSidebar() {
-  const [open, setOpen] = useState(false);
+interface DashboardMobileSidebarProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export default function DashboardMobileSidebar({ open, onClose }: DashboardMobileSidebarProps) {
   const pathname = usePathname();
 
   const isCoursePage =
@@ -16,16 +19,9 @@ export default function DashboardMobileSidebar() {
 
   return (
     <>
-      <button
-        onClick={() => setOpen(true)}
-        className="fixed left-4 top-4 z-50 flex h-10 w-10 items-center justify-center rounded-xl bg-white shadow-md lg:hidden"
-      >
-        <Menu size={22} />
-      </button>
-
       {open && (
         <div
-          onClick={() => setOpen(false)}
+          onClick={onClose}
           className="fixed inset-0 z-40 bg-black/40 lg:hidden"
         />
       )}
@@ -39,17 +35,17 @@ export default function DashboardMobileSidebar() {
         `}
       >
         <div className="flex justify-end p-4">
-          <button onClick={() => setOpen(false)} className="rounded-lg p-2 hover:bg-gray-100">
+          <button onClick={onClose} className="rounded-lg p-2 hover:bg-gray-100">
             <X size={22} />
           </button>
         </div>
 
         {isCoursePage ? (
           <div className="flex-1 overflow-y-auto">
-            <CourseFilters showLogo={false} onNavigate={() => setOpen(false)} />
+            <CourseFilters showLogo={false} onNavigate={onClose} />
           </div>
         ) : (
-          <Sidebar onNavigate={() => setOpen(false)} />
+          <Sidebar onNavigate={onClose} />
         )}
       </aside>
     </>
